4b700d096c205cb77a9155866500a4970c5dd831
zyedtu <zyedtu@yahoo.fr>
2020-10-15 09:35:07 +0200

    first commit

---
+++ bin/ocp/chapiter04/README.md
@@ -0,0 +1,11 @@
+									MAKING DECISIONS
+									
+# Création d'énoncés décisionnels: (Creating Decision-Making Statements)
+
+##### Déclarations et blocs:(Statements and Blocks)
+
+##### L'instruction if: (The if statement)
+								
+##### L'instruction else: (The else statement)
+
+##### L'instruction switch: (The switch statement)
---
+++ src/ocp/chapiter02/OcpTest2.java
@@ -0,0 +1,27 @@
+package ocp.chapiter02;
+
+public class OcpTest2 {
+
+	public static void main(String[] args) {
+
+		boolean b = true;
+		byte a = 12; // 8-bit
+		short x = 156;// 16-bit
+		int n = -45;// 32-bit
+		long l = 1238L;// 64-bit
+		float f = 15.14f;// 32-bit floating-point
+		double d = 1478.489;// 64-bit floating-point
+		char c = 'h';// 16-bit unicode value
+		
+		short bird = 'd';
+		char mammal = (short) 83;
+		System.out.println(bird);
+		System.out.println(mammal);
+		
+		int amount = 0156;
+		System.out.println(amount);
+
+
+	}
+
+}
